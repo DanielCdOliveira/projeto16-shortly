9d155d1c96347b1817be1adbef49e408ce5397c1
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-06-09 11:25:26 -0300

    POST /urls/shorten FIX

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -4,23 +4,17 @@ import { nanoid } from 'nanoid'
 import connection from "./../db.js";
 
 export async function createUrl(req, res) {
-    console.log(req.body);
     const {url} = req.body
     const shortUrl = nanoid()
-    console.log(shortUrl);
-    const { authorization } = req.headers;
-    const token = authorization?.replace("Bearer ", "").trim();
-    console.log(token);
+    const  {authorization}  = req.headers;
+    const token = authorization?.replace('Bearer ', '').trim();
     if (!token) return res.sendStatus(401);
   try {
-      const userId = 1
-    // const userId = await connection.query(`
-    // SELECT sessions."userId" 
-    // FROM sessions
-    // WHERE token=($1)     
-    // `,[token])
-
-    console.log(userId);
+    const {userId} = (await connection.query(`
+    SELECT sessions."userId" 
+    FROM sessions
+    WHERE token=($1)     
+    `,[token])).rows[0]
     if(!userId) return res.sendStatus(401)
     await connection.query(`
     INSERT INTO urls (url,"shortUrl","userId") 
@@ -34,10 +28,16 @@ res.status(201).send({shortUrl})
 
 }
 
-export async function getUrl(req, res) {
-
+export async function getUrlById(req, res) {
+    const id = req.params.id
+    console.log(id);
   try {
-    
+    const urlResult =  await connection.query(`
+    SELECT "id", "url", "shortUrl" 
+    FROM urls
+    WHERE id = $1    
+    `,[id])
+    console.log(urlResult);
   } catch (error) {
     console.log("Error recovering user.");
     console.log(error);
--- src/routers/urlsRouter.js
+++ src/routers/urlsRouter.js
@@ -1,11 +1,11 @@
 import {Router} from "express";
 
-import { createUrl } from "../controllers/urlsController.js";
+import { createUrl, getUrlById } from "../controllers/urlsController.js";
 
 
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten",  createUrl);
-
+urlRouter.get("/urls/:id",  getUrlById);
 
 export default urlRouter;
