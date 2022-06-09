7c940e52e3fb852b8f98a095f6509bdb92dffa26
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-06-09 11:52:56 -0300

    GET urls/open/:shortUrl

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -1,49 +1,62 @@
-
-import { nanoid } from 'nanoid'
+import { nanoid } from "nanoid";
 
 import connection from "./../db.js";
 
 export async function createUrl(req, res) {
-    const {url} = req.body
-    const shortUrl = nanoid()
-    const userId = res.locals.userId
-    console.log(userId);
-    // const  {authorization}  = req.headers;
-    // const token = authorization?.replace('Bearer ', '').trim();
-    // if (!token) return res.sendStatus(401);
+  const { url } = req.body;
+  const shortUrl = nanoid();
+  const userId = res.locals.userId;
   try {
-    // const {userId} = (await connection.query(`
-    // SELECT sessions."userId" 
-    // FROM sessions
-    // WHERE token=($1)     
-    // `,[token])).rows[0]
-    // if(!userId) return res.sendStatus(401)
-    await connection.query(`
+    await connection.query(
+      `
     INSERT INTO urls (url,"shortUrl","userId") 
     VALUES ($1,$2,$3)    
-    `,[url, shortUrl, userId])
-res.status(201).send({shortUrl})
+    `,
+      [url, shortUrl, userId]
+    );
+    res.status(201).send({ shortUrl });
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
   }
-
 }
 
 export async function getUrlById(req, res) {
-    const id = req.params.id
-    console.log(id);
+  const id = req.params.id;
+  console.log(id);
   try {
-    const urlResult =  (await connection.query(`
+    const urlResult = (
+      await connection.query(
+        `
     SELECT "id", "url", "shortUrl" 
     FROM urls
     WHERE id = $1    
-    `,[id])).rows[0]
-    if(!urlResult) return res.sendStatus(404)
-    res.status(200).send(urlResult)
+    `,
+        [id]
+      )
+    ).rows[0];
+    if (!urlResult) return res.sendStatus(404);
+    res.status(200).send(urlResult);
   } catch (error) {
     console.log("Error recovering user.");
     console.log(error);
     return res.sendStatus(500);
   }
+}
+
+export async function redirectToUrl(req,res){
+    const shortUrl = req.params.shortUrl
+try {
+    const {url} = (await connection.query(`
+    SELECT url 
+    FROM urls 
+    WHERE "shortUrl"=$1    
+    `,[shortUrl])).rows[0]
+    console.log(url);
+    if(!url) return res.sendStatus(404)
+    res.redirect(url)
+
+} catch (error) {
+    return res.sendStatus(500)
+}
 }
--- src/routers/urlsRouter.js
+++ src/routers/urlsRouter.js
@@ -1,11 +1,12 @@
 import {Router} from "express";
 
-import { createUrl, getUrlById } from "../controllers/urlsController.js";
+import { createUrl, getUrlById, redirectToUrl } from "../controllers/urlsController.js";
 import tokenValidation from "../middlewares/tokenValidation.js";
 
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten",tokenValidation,  createUrl);
 urlRouter.get("/urls/:id",  getUrlById);
+urlRouter.get("/urls/open/:shortUrl",  redirectToUrl);
 
 export default urlRouter;
