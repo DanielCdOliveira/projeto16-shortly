31b8b5eed5c00088665914274d8d48aa683c72df
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-06-10 11:53:04 -0300

    fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -20,7 +20,7 @@ export async function signUp(req, res) {
     return res.sendStatus(201);
   } catch (error) {
     if (error.code === "23505") return res.status(409).send(error.detail);
-    return res.sendStatus(500);
+    return res.status(500).send(error);
   }
 }
 
